ddb1c6289d18df8acef622e9739d5919c98bbc53
unknown <jiyongseong@hotmail.com>
2020-06-18 16:41:51 +0900

    select-string

--- RND/Network/Get-NSG-Rules.ps1
+++ RND/Network/Get-NSG-Rules.ps1
@@ -2,9 +2,10 @@
 $rg = "RG-MO"
 $nsg_name = "MO-NSG"
 $rule_name = "Allow_From_OnPrem_To_P4R_TCP"
+$keyword = "22"
 
 $rule = Get-AzureRmNetworkSecurityGroup -Name $nsg_name -ResourceGroupName $rg | Get-AzureRmNetworkSecurityRuleConfig -Name $rule_name
 
-$rule.SourceAddressPrefix
-$rule.DestinationAddressPrefix
-$rule.DestinationPortRange
+$rule.SourceAddressPrefix | Select-String $keyword 
+$rule.DestinationAddressPrefix | Select-String $keyword
+$rule.DestinationPortRange | Select-String $keyword
